94621034f1e16d50cf414533d0ea8cca756631dd
Philkyue Shin <pkshin@redwood.snu.ac.kr>
2022-01-14 16:27:08 +0900

    Update run_mAP.sh

--- run_mAP.sh
+++ run_mAP.sh
@@ -2,7 +2,7 @@ export LD_LIBRARY_PATH=lib/:snpe-1.58/lib/
 export ADSP_LIBRARY_PATH=snpe-1.58/lib/
 
 echo "Detecting images.."
-./pkshin_detect image model/ssd_mobilenet_v2.dlc coco_labels.txt coco_val2017 detection_result.json npu
+./pkshin_detect image model/ssd_mobilenet_v1.dlc coco_labels.txt coco_val2017 detection_result.json npu
 
 if [ $? -eq 1 ];
 then
