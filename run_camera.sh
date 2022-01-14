db7a00f93b8766ffab9027a3fff2423fb4e444e9
Philkyue Shin <pkshin@redwood.snu.ac.kr>
2022-01-14 16:26:58 +0900

    Update run_camera.sh

--- run_camera.sh
+++ run_camera.sh
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH=lib/:snpe-1.58/lib/
 export ADSP_LIBRARY_PATH=snpe-1.58/lib/
 
-./pkshin_detect camera model/ssd_mobilenet_v2.dlc coco_labels.txt ssdDisplay.xml npu
+./pkshin_detect camera model/ssd_mobilenet_v1.dlc coco_labels.txt ssdDisplay.xml npu
